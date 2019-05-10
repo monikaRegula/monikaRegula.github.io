e34d7aacb1f0a8cbc262b9431e04754533a773d0
Monika <monikaregula97@gmail.com>
2019-05-10 21:37:45 +0200

    apiRoot .... /task/

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  var apiRoot = 'https://sleepy-coast-90756.herokuapp.com/v1/trello/';
+  var apiRoot = 'https://sleepy-coast-90756.herokuapp.com/v1/task/';
+
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
