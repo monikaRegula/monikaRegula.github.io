7d8514ecd0a3226a7fc1a7d8a1a008f70fc3eb8d
Monika <monikaregula97@gmail.com>
2019-05-10 21:46:37 +0200

    dodanie trelloApiRoot dla zewn

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var apiRoot = 'https://sleepy-coast-90756.herokuapp.com/v1/task/';
-
+  var trelloApiRoot = 'https://sleepy-coast-90756.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
